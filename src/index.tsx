d3d174d5926f832d69e092b25302843751dc2dcb
anastasiya.berezhnaya <anastasiya.berezhnaya@wilix.org>
2021-03-30 13:11:58 +0300

    dummy commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import { createStore } from 'redux';
 import App from "./App";
 import { Provider } from "react-redux";
